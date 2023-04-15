ed518fe95220dc220238d8c96d8e3fb278f78970
Pablo-Vit <pablovitasse23@gmail.com>
2023-04-15 11:45:31 -0300

    tp1

---
+++ tp1/js/Funiones.js
@@ -0,0 +1,6 @@
+function miFuncion(){
+    const Apellido = document.getElementById('cajaA').value;
+    const Nombre = document.getElementById('cajaN').value;
+    const Edad = document.getElementById('cajaE').value;
+    alert("Nombre: "+Apellido+" "+Nombre+"\nEdad: "+Edad);
+}
--- tp1/script.js
+++
@@ -1,3 +0,0 @@
-function test(x){
-    console.log(x*9+10)
-}
