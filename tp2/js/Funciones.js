56503785007d0d4cf09cd760c0fbfcb116de3aaa
Pablo-Vit <129400929+Pablo-Vit@users.noreply.github.com>
2023-04-28 18:55:50 -0300

    Add files via upload

---
+++ tp2/js/Funciones.js
@@ -0,0 +1,11 @@
+function getJugador(){
+    let Jugador = {
+        Nombre:'Nahuel',
+        Apellido:'Barrios',
+        Club:'San Lorenzo',
+        Apodo:'Perrito',
+        Edad: 23,
+        Activo: true,
+        Historial: ['ni idea']
+    }
+}
