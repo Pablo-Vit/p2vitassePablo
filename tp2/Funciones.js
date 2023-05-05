ccd9f9fe64f77cc49f1aa642e5ad10c904df4378
Pablo-Vit <pablovitasse23@gmail.com>
2023-05-05 15:27:47 -0300

    tp2

---
+++ tp2/Funciones.js
@@ -0,0 +1,16 @@
+function getCharacter(){
+    let id = document.getElementById("CharBox").value
+    fetch("https://rickandmortyapi.com/api/character/"+id)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            
+            document.getElementById("name").innerText = data.name;
+            document.getElementById("status").innerText = data.status;
+            document.getElementById("specie").innerText = data.species;
+            document.getElementById("city").innerText = data.location.name;
+            document.getElementById("foto").scrset = data.image;
+            console.log(data);
+        })
+}
