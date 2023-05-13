4a2e2df426b603c39a701f144f7f7b2dd722872d
Pablo-Vit <pablovitasse23@gmail.com>
2023-05-13 13:03:35 -0300

    tp3

--- tp3/JS.js
+++ tp3/JS.js
@@ -1,43 +1,92 @@
 let pagAct = 0;
 let info;
-function getCharacter(x) {
-    let id = document.getElementById("CharBox"+x).value;
-    id = parseInt(id);
-    pag = parseInt(id / 20) + 1;
-    let rId = (id % 20) - 1;
-    if (pag == pagAct) {
-        console.log('No Fetch');
-        let dat = info.results[rId]
-        document.getElementById("name"+x).innerText = dat.name;
-        document.getElementById("status"+x).innerText = dat.status;
-        document.getElementById("specie"+x).innerText = dat.species;
-        document.getElementById("city"+x).innerText = dat.location.name;
-        document.getElementById("foto"+x).innerHTML = ` <img src="` + dat.image + `">`;
-    } else {
-        console.log('Fecth');
-        fetch("https://rickandmortyapi.com/api/character/?page=" + pag)
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (data) {
-                let dat = data.results[rId]
-                info = data;
-                console.log(dat);
-                document.getElementById("name"+x).innerText = dat.name;
-                document.getElementById("status"+x).innerText = dat.status;
-                document.getElementById("specie"+x).innerText = dat.species;
-                document.getElementById("city"+x).innerText = dat.location.name;
-                document.getElementById("foto"+x).innerHTML = ` <img src="` + dat.image + `">`;
-                pagAct = pag;
-            })
-            .catch(function (error) {
-                document.getElementById("name"+x).innerText = "Error: Personaje invalido";
-                document.getElementById("status"+x).innerText = "";
-                document.getElementById("specie"+x).innerText = "";
-                document.getElementById("city"+x).innerText = "";
-                document.getElementById("foto"+x).innerHTML = "";
-                console.log(error)
-            })
+let cols = 2;
+let eps = [];
+let names = [];
+function getCharacters() {
+    for(let i = 1; i < cols+1; i++)
+    {
+        let id = document.getElementById("CharBox"+i).value;
+        id = parseInt(id);
+        let pag = parseInt(id / 20) + 1;
+        let rId = (id % 20) - 1;
+        if (pag == pagAct) {
+            if(id > 0)
+            {
+                let dat = info.results[rId]
+                document.getElementById("name"+i).innerText = dat.name;
+                document.getElementById("status"+i).innerText = dat.status;
+                document.getElementById("specie"+i).innerText = dat.species;
+                document.getElementById("city"+i).innerText = dat.location.name;
+                document.getElementById("foto"+i).innerHTML = ` <img src="` + dat.image + `">`;
+                eps.push(dat.episode.length);
+                names.push(dat.name);
+                comparar();
+            } else{
+                document.getElementById("name"+i).innerText = "Error: Personaje invalido";
+                document.getElementById("status"+i).innerText = "";
+                document.getElementById("specie"+i).innerText = "";
+                document.getElementById("city"+i).innerText = "";
+                document.getElementById("foto"+i).innerHTML = "";
+            }
+        } else {
+            fetch("https://rickandmortyapi.com/api/character/?page=" + pag)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    let dat = data.results[rId]
+                    info = data;
+                    console.log(dat);
+                    document.getElementById("name"+i).innerText = dat.name;
+                    document.getElementById("status"+i).innerText = dat.status;
+                    document.getElementById("specie"+i).innerText = dat.species;
+                    document.getElementById("city"+i).innerText = dat.location.name;
+                    document.getElementById("foto"+i).innerHTML = ` <img src="` + dat.image + `">`;
+                    pagAct = pag;
+                    eps.push(dat.episode.length);
+                    names.push(dat.name);
+                    comparar();
+                })
+                .catch(function (error) {
+                    document.getElementById("name"+i).innerText = "Error: Personaje invalido";
+                    document.getElementById("status"+i).innerText = "";
+                    document.getElementById("specie"+i).innerText = "";
+                    document.getElementById("city"+i).innerText = "";
+                    document.getElementById("foto"+i).innerHTML = "";
+                    console.log(error)
+                })
+        }
     }
+}
 
+let con = 0;
+function comparar()
+{
+    con++;
+    if(con == cols){
+        let maxeps = 0;
+        for(let i = 0; i < cols; i++){
+            if(maxeps < eps[i]){
+                maxeps=eps[i];
+            }
+        }
+        con = 0;
+        let iguales = 0;
+        let igualNames = '';
+        for(let i = 0; i < cols; i++){
+            if(maxeps == eps[i]){
+                iguales++;
+                if(iguales > 1) {igualNames+=' y '+names[i];}
+                else {igualNames+=names[i];}
+            }   
+        }
+        if(iguales == 1){
+            document.getElementById("qdm").innerText = igualNames+' aparece en mas episodios con '+ maxeps;
+        } else if(iguales > 1) {
+            document.getElementById("qdm").innerText = 'Aparecen en la misma cantidad de episodios '+igualNames+' con '+ maxeps;
+        }
+        eps.length = 0;
+        names.length = 0;
+    }
 }
